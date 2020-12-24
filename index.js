0f9b16ce8ffedbc3430686fe975eebbc7f5ddb7d
avanthikag <avanthikaa2424@gmail.com>
2020-12-24 21:29:18 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+
+import './style.css';
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1><form action="action_page.php" style="border:1px solid #ccc">
+  <div class="container">
+    <hr>
+
+    <label for="email"><b>Email</b></label>
+    <input type="text" placeholder="Enter Email" name="email" required>
+
+    <label for="psw"><b>Password</b></label>
+    <input type="password" placeholder="Enter Password" name="psw" required>
+
+    <label for="psw-repeat"><b>Repeat Password</b></label>
+    <input type="password" placeholder="Repeat Password" name="psw-repeat" required>
+  
+  
+
+    <div class="clearfix">
+      <button type="button" class="cancelbtn">Cancel</button>
+      <button type="submit" class="signupbtn">Sign Up</button>
+    </div>
+  </div>
+</form>
+</h1>`;
